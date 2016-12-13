5290cb374d471f0c28cd7fd9922e71f68446bad4
surabhirk <surabhikulkarni1010@gmail.com>
2016-12-13 11:22:50 +0530

    test

--- src/com/shopping/test/businesslayer/ProductBusinessLayerTest.java
+++ src/com/shopping/test/businesslayer/ProductBusinessLayerTest.java
@@ -5,20 +5,33 @@ package com.shopping.test.businesslayer;
 
 import static org.junit.Assert.*;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import org.junit.Test;
+
+import com.shopping.bean.ProductBean;
+import com.shopping.businesslayer.ProductBusinessLayer;
+import com.shopping.constants.ShoppingConstants;
 
 /**
  * @author singhkri
  *
  */
-public class ProductBusinessLayerTest {
+public class ProductBusinessLayerTest
+{
+	ProductBusinessLayer productBusinessLayer;
+	ProductBean productBean =null;
 
 	/**
 	 * Test method for {@link com.shopping.businesslayer.ProductBusinessLayer#productDetailsGetter(com.shopping.bean.ProductBean)}.
+	 * @throws SQLException 
 	 */
 	@Test
-	public final void testProductDetailsGetter() {
-		fail("Not yet implemented"); // TODO
+	public final void testProductDetailsGetter() throws SQLException 
+	{
+		assertEquals(ShoppingConstants.list,productBusinessLayer.productDetailsGetter(productBean));
+
 	}
 
 }
