ae966f09fa3ab7b882c1ae41e7f6cd5182617ee3
krishna1994 <singhkrishna1994@gmail.com>
2016-12-15 15:08:12 +0530

    LoginDAOtest change

--- src/com/shopping/test/dao/LoginDAOTest.java
+++ src/com/shopping/test/dao/LoginDAOTest.java
@@ -1,43 +1,75 @@
-/**
- * 
- */
 package com.shopping.test.dao;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Test;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import com.mysql.jdbc.PreparedStatement;
 import com.shopping.bean.LoginBean;
-import com.shopping.constants.ShoppingConstants;
 import com.shopping.dao.LoginDAO;
+import com.shopping.util.DBConnection;
 
 /**
  * @author singhkri
  *
  */
-public class LoginDAOTest
-{
+public class LoginDAOTest {
+
+	@InjectMocks 
+	LoginDAO loginDAO;
+
+	@Mock 
+	DBConnection dbConnection;
+	
+	@Mock
+	Connection connection;
+	
+	@Mock
+	ResultSet resultSet;
+	
+	@Mock
+	PreparedStatement preparedStatement;
+
+	
+	
+	
+	
+	@Before
+	public void setUp() throws SQLException 
+	{
+		MockitoAnnotations.initMocks(this);
+		
+		Mockito.when(dbConnection.createConnection()).thenReturn(connection);
+		Mockito.when(resultSet.next()).thenReturn(true, false);
+		Mockito.when(connection.prepareStatement(Mockito.anyString())).thenReturn(preparedStatement);
+		Mockito.when(preparedStatement.executeQuery()).thenReturn(resultSet);
+		//Mockito.when(loginDAO.authenticateUser((LoginBean)Mockito.anyObject())).thenReturn(resultSet);
 
 
+	
+	}
 
 	@Test
-	public void authenticateUserTest()
+	public void testAuthenticateUser() throws Exception 
 	{
-		LoginDAO loginDAO = new LoginDAO();
+		ResultSet loginSet  = loginDAO.authenticateUser(new LoginBean());
+        assertEquals(loginSet.next(), true);
+        assertEquals(loginSet.next(), false);
 
-		LoginBean loginBean ;
-
-		loginBean = new LoginBean();
-		loginBean.setPassword("1234");
-		loginBean.setUsername(null);
-		assertEquals(ShoppingConstants.INVALID_CREDENTIALS,loginDAO.authenticateUser(loginBean));
-
-		loginBean.setPassword(null);
-		loginBean.setUsername("abc");
-		assertEquals(ShoppingConstants.INVALID_CREDENTIALS,loginDAO.authenticateUser(loginBean));
-
-
-
+       
+        Mockito.verify(preparedStatement.executeQuery(), Mockito.times(1));
+		
+		
 	}
+	
 
 }
