065b9c42ff6828eaac051ce283218847d0a2d880
surabhirk <surabhikulkarni1010@gmail.com>
2016-12-22 12:16:58 +0530

    cartItemsDao changes

--- src/com/shopping/dao/CartItemsDAO.java
+++ src/com/shopping/dao/CartItemsDAO.java
@@ -12,51 +12,52 @@ public class CartItemsDAO
 {
 	Connection con = null;		//  Reference to connection object is deleted
 	PreparedStatement preparedStatement = null;		//Reference to Preparedstatement object is deleted
-	ResultSet resultSet = null;		 //Reference to ResultSet object is deleted
+	ResultSet resultSet = null;		 //Refrence to ResultSet object is deleted
 	int i;
-	CartItemsBean cartItemsBean=new CartItemsBean();
-	public  int addItems() throws SQLException
+	
+	public ResultSet checkCartId() throws SQLException
 	{
-		int quantity=cartItemsBean.getQuantity();
-		try 
-		{
+		con=DBConnection.createConnection();
+		String query="SELECT MAX(cartId) from cart ";
+		preparedStatement=con.prepareStatement(query);
+		resultSet=preparedStatement.executeQuery();
+		
+		return resultSet;
+		
+	}
+	
+	public  int addItems(CartItemsBean cartItemsBean) throws SQLException
+	{
+		
 			con = DBConnection.createConnection();	 // calling createConnection of DBConnectuion class in con
-			String query="insert into cartitems(username,productId, quantity) values(?,?,?)";
+			String query="insert into cartitems (CartId ,ProductId , Quantity) values (? ,? ,?)";
 			preparedStatement = con.prepareStatement(query);	//query to be executed
-			preparedStatement.setString(1, "username");
-			preparedStatement.setString(2,"productId" );
-			preparedStatement.setInt(3, quantity);
+			preparedStatement.setInt(1, cartItemsBean.getCartId());
+			preparedStatement.setString(2, cartItemsBean.getProductId());
+			preparedStatement.setInt(3, cartItemsBean.getQuantity());
 			i=preparedStatement.executeUpdate();
-		}
-		finally
-		{
 
 
-		}
-		System.out.println(" CartItems DAO:"+resultSet.toString());
+
+		
+		//System.out.println(" CartItems DAO:"+resultSet.toString());
+
+
+
 		return i;
 	}
 
-	public ResultSet showcartItems() throws Exception
+	public ResultSet showcartItems() throws Exception 
 	{
-		try 
-		{
-			con = DBConnection.createConnection();	                            // calling createConnection of DBConnectuion class in con
-			preparedStatement = con.prepareStatement("select * from cartitems");	                    //query to be executed
-			resultSet = preparedStatement.executeQuery();	                   //saving execured query resultSet
-
-		}
-		finally
-		{
-
-			//				con.close();		//closing connection
-
-		}
-		System.out.println("DAO:"+resultSet.toString());
+		con = DBConnection.createConnection();	 // calling createConnection of DBConnectuion class in con
+		String query="SELECT * FROM cartitems";
+		preparedStatement = con.prepareStatement(query);	//query to be executed
+		resultSet = preparedStatement.executeQuery();	//saving execured query resultSet
+		
 		return resultSet;
 	}
-
-
-
+	
+	
+	 
 
 }
